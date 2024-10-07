ee39b38d53de759a3acfda7a1b67c0179d01f306
Sergey Piunov <piunov92@gmail.com>
2024-10-07 13:31:51 +0300

    fix user controller

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -4,21 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.dto.UserDto;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
-import ru.kata.spring.boot_security.demo.services.UserService;
 import ru.kata.spring.boot_security.demo.utils.Utils;
-import java.util.List;
 
 @Controller
 public class UserController {
-    private final UserService userService;
     private final RoleService roleService;
 
     @Autowired
-    public UserController(UserService userService, RoleService roleService) {
-        this.userService = userService;
+    public UserController(RoleService roleService) {
         this.roleService = roleService;
     }
 
@@ -32,21 +27,7 @@ public class UserController {
     @GetMapping("/admin")
     public String users(@ModelAttribute("user") User user, Model model) {
         Utils.auth(model);
-
-        model.addAttribute("users", userService.getAllUsers());
-        model.addAttribute("userDto", new UserDto());
         model.addAttribute("allRoles", roleService.getAllRoles());
-        model.addAttribute("error");
         return "admin/index";
     }
-
-//    @PostMapping("/admin/update/{id}")
-//    public String updateUser(@PathVariable Long id, @ModelAttribute("user") User user, @RequestParam List<String> roleNames, Model model) {
-//        try {
-//            userService.updateUser(id, user, roleNames);
-//            return "redirect:/admin";
-//        } catch (Exception ignored) {
-//        }
-//        return "redirect:/admin";
-//    }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/dto/UserDto.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dto/UserDto.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.kata.spring.boot_security.demo.models.Role;
 
 import java.util.Collection;
 
@@ -11,7 +10,6 @@ import java.util.Collection;
 @Setter
 @AllArgsConstructor
 public class UserDto {
-//    private String id;
     private String username;
     private String password;
     private String email;
